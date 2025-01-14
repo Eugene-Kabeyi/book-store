5394c1ad779c47aa20391cdb8427db44b0b13494
KABEYI <eugenekabeyi144@gmail.com>
2025-01-14 23:08:17 +0300

    register page made

---
+++ register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <!--font awesome cdn link-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <!-- custom csss file -->
+</head>
+<body>
+    <div class="form-container">
+        <form action="" method="post">
+            <input type="text" name="name" placeholder="enter your name" class="box">
+            <input type="email" name="email" placeholder="enter your email" class="box">
+            <input type="password" name="name" placeholder="enter your password" class="box">
+            <input type="password" name="name" placeholder="confirm your password" class="box">
+            <select name="user-type" id="">
+                <option value="user">user</option>
+                <option value="admin">admin</option>
+            </select>"
+            <input type="submit" name="submit" value= "register now" class="btn">
+            <p>already have an account</p> <a href="login.php">login now</a>
+        </form> 
+    </div>
+</body>
+</html>
